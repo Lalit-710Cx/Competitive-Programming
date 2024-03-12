da96301ad134a5e3f6abf2468cf14927e336774c
Bikram007 <code.bikram.007@gmail.com>
2024-03-12 19:51:39 +0530

    12/03/24

---
+++ Python/05_String_formatting.py
@@ -0,0 +1,15 @@
+#in this page i learn about how differnet sring format using fstring structure:
+name = "bikram"
+age  = 28
+print(f"hello my name is {name} and my age is {age}")
+
+
+name = "Soumya"
+age = 28
+print(f"hello my name is {name} and age is {age}")
+
+name3 = "Aparna"
+age3 = 23
+print(f"Hello My name is {name3} and age is {age3}")
+
+
---
+++ Python/06_String_scape.py
@@ -0,0 +1,10 @@
+escaped_string = "This is a line\nAnd this is the new linw"
+print(escaped_string)
+
+#if i don't need any ecape charecter in line
+raw_string = r"This is a raw string\nNew line won't be interpreted."
+print(raw_string)
+
+#Checking substring
+xxx = "i trying to understand the theory of god"
+print("the" in xxx)
